7efc23494fdbd09dd21ac99f0625f2b3f685d66b
Dario0122 <dario.di.cuia@gmail.com>
2021-10-20 14:49:01 +0200

    3 domande

---
+++ js/main.js
@@ -0,0 +1,17 @@
+// Password generator
+
+// Ottieni nome dell'utente
+const nomeUtente = prompt ('Qual è il tuo nome?')
+console.log(nomeUtente);
+console.log(typeof nomeUtente);
+
+// Ottieni cognome dell'utente
+const cognomeUtente = prompt ('Qual è il tuo cognome?')
+console.log(cognomeUtente);
+console.log(typeof cognomeUtente);
+
+// Chiedi qual è il suo colore preferito
+const coloreUtente = prompt ('Qual è il tuo colore preferito?')
+console.log(coloreUtente);
+console.log(typeof coloreUtente);
+
